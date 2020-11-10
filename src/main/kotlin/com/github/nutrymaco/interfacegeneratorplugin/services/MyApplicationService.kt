d9162396a71b2983229647b5fbe1875da497f0b7
GitHub Action <action@github.com>
2020-11-10 14:45:16 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/nutrymaco/interfacegeneratorplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.nutrymaco.interfacegeneratorplugin.services
+
+import com.github.nutrymaco.interfacegeneratorplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/nutrymaco/interfacegeneratorplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nutrymaco/interfacegeneratorplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.nutrymaco.interfacegeneratorplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nutrymaco.interfacegeneratorplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
